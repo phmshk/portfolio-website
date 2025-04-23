07ecedc20c36dd1ebb6600be4b7dfea1622ad0cd
Philipp <ph.moshkov@gmail.com>
2025-04-23 08:39:47 +0200

    feat: added styling to header and about section

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ import { delay } from "./printMessages.js";
 
 const rows = document.querySelector(".console-rows");
 const consoleSection = document.querySelector(".console-section");
-const themeToggleBtn = document.querySelector(".header__theme-toggle-btn");
-const themeIcon = document.querySelector(".header__theme-icon");
+const toggle = document.getElementById("themeToggle");
+const html = document.documentElement;
 
 const messages = [
   " Hello, visitor.",
@@ -13,31 +13,24 @@ const messages = [
   " Done. Enjoy your stay!",
 ];
 
+toggle.addEventListener("click", () => {
+  const isDark = html.getAttribute("data-theme") === "dark";
+  html.setAttribute("data-theme", isDark ? "light" : "dark");
+  toggle.classList.toggle("dark", !isDark);
+});
+
 async function switchToPageView(ms) {
   await printMessages(messages, rows);
   await delay(ms);
   consoleSection.classList.add("fade-away");
 }
 
-//switchToPageView(2000);
-consoleSection.style.display = "none";
+async function startInitialAnimation() {
+  document.body.style.overflow = "hidden";
+  await switchToPageView(2000);
+  document.body.style.overflow = "";
+  setTimeout(() => (consoleSection.style.display = "none"), 800);
+}
 
-// themeToggleBtn.addEventListener("click", () => {
-//   const currentTheme = document.documentElement.getAttribute("data-theme");
-//   const newTheme = currentTheme === "dark" ? "light" : "dark";
-//   document.documentElement.setAttribute("data-theme", newTheme);
-//   if (currentTheme == "light") {
-//     themeIcon.classList.replace("fa-moon", "fa-sun");
-//   } else {
-//     themeIcon.classList.replace("fa-sun", "fa-moon");
-//   }
-// });
-
-const toggle = document.getElementById("themeToggle");
-const html = document.documentElement;
-
-toggle.addEventListener("click", () => {
-  const isDark = html.getAttribute("data-theme") === "dark";
-  html.setAttribute("data-theme", isDark ? "light" : "dark");
-  toggle.classList.toggle("dark", !isDark);
-});
+//startInitialAnimation();
+consoleSection.hidden = true;
